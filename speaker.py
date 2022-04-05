11239f465193f1af5da0c0c507faba0056ccd62e
David Graper <dgraper@albany.edu>
2022-04-05 09:45:06 -0400

    Third push

---
+++ speaker.py
@@ -0,0 +1,28 @@
+import pyttsx3
+
+speaker = pyttsx3.init()
+
+
+
+speaker.say('Good morning Dave.  You have eight new emails.')
+speaker.say('Last night I sensed a hard drive fault in server 10.221.19.21')
+speaker.say('You can check the log from 11:33 p.m. to see the details.')
+speaker.say('Both will go 100 percent fail in 72 hours.')
+speaker.say('There was a human visitor in this office at 10:46 p.m. last night.  That person entered by the main lobby door and was in this office for 4 minutes and 33 seconds..')
+
+speaker.say('New Baltimore camera 1 reports a human visitor on the driveway camera at 11:15 a.m. this morning')
+speaker.say('New Baltimore camera 2 reported unknown movement at 5:13 p.m.')
+speaker.say('New Baltimore camera 3 reporting movement at 1:00 p.m., 2:33 p.m., and 5:14 p.m.')
+
+speaker.say('Ralph Waldo Emerson once said, When it comes time to divide an estate, the politest men quarrel.')
+speaker.say('Keep in mind that no matter how much you want to compromise, make everybody feel good, and make minor accommodations, you are unlikely to please everyone.')
+speaker.say('A thick skin during this time would be your best asset.')
+speaker.say('This is especially true when there is more than one beneficiary.')
+speaker.say('There will be conflicting opinions, different views of what is right and what is wrong, different time schedules, motivations, and so on.')
+speaker.say('It will probably be the case that all the views are a little right and a little wrong.')
+speaker.say("You can't please everyone. You should not try to please anyone.")
+speaker.say("You're going to have competing interests through the entire process, so you’d better get used to it right up front.")
+speaker.say("Just try to do what you think is right.")
+
+speaker.say("Shave and a haircut.  Two bits.")
+speaker.runAndWait()
